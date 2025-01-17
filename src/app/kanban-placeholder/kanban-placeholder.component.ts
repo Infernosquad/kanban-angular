fd21f0fe94474f696d46eccabd02f6981b00cd91
infernosquad <infzanoza@gmail.com>
2025-01-17 07:59:36 +0400

    Add first signal

--- src/app/kanban-placeholder/kanban-placeholder.component.ts
+++ src/app/kanban-placeholder/kanban-placeholder.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, input} from '@angular/core';
 
 @Component({
   selector: 'app-kanban-placeholder',
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './kanban-placeholder.component.scss'
 })
 export class KanbanPlaceholderComponent {
-
+  title = input('');
 }
