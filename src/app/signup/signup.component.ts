2ad477419450d444050ad901573d3135418e152e
infernosquad <infzanoza@gmail.com>
2025-01-24 08:38:57 +0400

    Add signup boilerplate

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Button } from 'primeng/button';
+import { FormsModule } from '@angular/forms';
+import { Dialog } from 'primeng/dialog';
 
 @Component({
   selector: 'app-signup',
-  imports: [],
+  imports: [
+    Button,
+    FormsModule,
+    Dialog
+  ],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss'
 })
 export class SignupComponent {
+  displaySignup: boolean = true;
+  username: string = '';
+  password: string = '';
+  email: string = '';
 
+  @Output() displaySignupChange = new EventEmitter<boolean>();
+
+  hideSignup() {
+    this.displaySignupChange.emit(false);
+  }
+
+  signup() {
+    // Implement signup logic here
+    console.log('Username:', this.username);
+    console.log('Password:', this.password);
+    console.log('Email:', this.email);
+  }
 }
